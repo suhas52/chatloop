07b13fc17c8ca7788471372b640b02074cd515aa
suhas52 <suhasedu991@gmail.com>
2025-11-05 14:33:32 +0530

    send data from the endpoints to the frontend with res.json and wrap the endpoints in try/catch blocks for error handling

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -8,23 +8,32 @@ const JWT_SECRET: string = String(process.env.JWT_SECRET);
 
 authRouter.post("/register", (req, res) => {
     const {username, password, first_name, last_name} = req.body;
-    registerUser(username, password, first_name.charAt(0).toUpperCase() + first_name.slice(1), last_name.charAt(0).toUpperCase() + last_name.slice(1));
-    res.status(201).json({success: "User added"})
+    try {
+        registerUser(username, password, first_name.charAt(0).toUpperCase() + first_name.slice(1), last_name.charAt(0).toUpperCase() + last_name.slice(1));
+        res.status(201).json({message: "User added"})
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({message: "Failed to add User."})
+    }
 })
 
 authRouter.post("/login", async (req, res) => {
     const {username, password} = req.body;
-    const user_id = await loginUser(username, password);
-    if (user_id) {
-        const token = jwt.sign({user_id: user_id, username: username}, JWT_SECRET);
-        res.status(200).
-        cookie("token", token, {
-            httpOnly: true,
-            maxAge: 1000 * 60 * 60 * 24
-        })
-        .json({ message: "Login successful" });
-    } else {
-        res.status(400).json({error: "Wrong password"})
+    try {
+        const user_id = await loginUser(username, password);
+        if (user_id) {
+            const token = jwt.sign({user_id: user_id, username: username}, JWT_SECRET);
+            res.status(200).
+            cookie("token", token, {
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60 * 24
+            })
+            .json({ message: "Login successful" });
+        } else {
+            res.status(400).json({message: "Wrong password"})
+        }
+    } catch (err) {
+        res.status(500).json({message: "Unable to sign in."})
     }
 })
 
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -7,24 +7,52 @@ import { start } from 'repl';
 
 export const userRouter = express.Router();
 userRouter.get("/me", verifyToken, async (req: any, res: Response) => {
-    const user = await getUser(req.user.user_id);
-    console.log(user)
+    try {
+        const user = await getUser(req.user.user_id);
+        res.status(200).json(user)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message: "Failed to fetch user."})
+    }
+    
 })
 
 userRouter.get("/conversations/:id", verifyToken, async (req: any, res: Response) => {
-    getConversations(req.params.id)
+    try {
+        const conversations = await getConversations(req.params.id);
+        res.status(200).json(conversations)
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({message: "Request failed."})
+    }
 })
 
 userRouter.get("/messages/:id", verifyToken, async (req: any, res: Response) => {
-    getMessages(req.params.id)
+    try { 
+        const messages = await getMessages(req.params.id)
+        res.status(200).json(messages)
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({message: "Request failed."})
+    }
 })
 
 userRouter.get("/users", verifyToken, async (req: any, res: Response) => {
-    getAllUsers();
+    try {
+        const users = await getAllUsers();
+        res.status(200).json(users);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({message: "Request failed."})
+    }
 })
 
 userRouter.post("/conversation", verifyToken, async (req: any, res: Response) => {
     const { userA, userB } = req.body;
-
-    startNewConversation(userA, userB);
+    try { await startNewConversation(userA, userB);
+        res.status(200).json({message: "Successfully created conversation."})
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({message: "Failed to create conversation"})
+    }
 })
